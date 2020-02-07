166fbefb181da55d881e736a4af6633e4d959bd6
Thomas Bridge <thomas.bridge@fiserv.com>
2020-02-07 12:59:29 +0000

    First attempt at script to start EC2 instances based on tag

---
+++ start_ec2_platform/start_ec2_platform.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+import boto3
+import argparse
+from pprint import pprint
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--role", help="Rolename in configuration")
+    parser.add_argument("-p", "--platform", help="Platform value in tag", default="Linux")
+    parser.add_argument("-a", "--action", help="start|stop|status", default="status")
+    return(parser)
+
+# if defined(role)
+def main():
+    args = createParser().parse_args()
+    platform = args.platform
+    role = args.role
+    action = args.action
+    print("Action is " + action)
+    if action not in ['start', 'stop', 'status']:
+        print("Invalid action")
+        sys.exit()
+
+    try:
+        ec2 = boto3.Session(profile_name=role).client('ec2')
+        filter = [{'Name': 'tag:Platform', 'Values': [platform]}]
+        instances = ec2.describe_instances(Filters=filter)
+    except Exception as e:
+        print("Error: ", e)
+        sys.exit()
+
+    if instances['ResponseMetadata']['HTTPStatusCode'] == 200:
+        for x in instances['Reservations']:
+            inst_id = x['Instances'][0]['InstanceId']
+            state = x['Instances'][0]['State']
+            if action == "status":
+                print(inst_id, state)
+            elif action == "stop":
+                # Stop if running else warn and do nothing.
+                if state['Code'] == 16:
+                    print("Stopping instance ", inst_id)
+                    ec2.stop_instances(InstanceIds=[inst_id])
+                else:
+                    print(inst_id, " has status ", state['Name'], " doing nothing")
+            elif action == "start":
+                if state['Code'] == 80:
+                    print("Starting instance ", inst_id)
+                    ec2.start_instances(InstanceIds=[inst_id])
+                else:
+                    print(inst_id, " has status ", state['Name'], " doing nothing")
+    else:
+        print("HTTP Response not correct")
+# pprint(instances)
+
+if __name__ == '__main__':
+    main()
